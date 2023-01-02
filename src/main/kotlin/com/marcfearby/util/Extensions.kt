ac2c4f29f9126d4d7e11dbe90453ec5b17c517a3
Marc Fearby <marc.fearby@service.nsw.gov.au>
2023-01-02 14:01:04 +1100

    Add a record to the table

--- src/main/kotlin/com/marcfearby/util/Extensions.kt
+++ src/main/kotlin/com/marcfearby/util/Extensions.kt
@@ -6,3 +6,7 @@ import java.time.LocalDate
 fun DateTime.toJavaLocalDate(): LocalDate {
     return LocalDate.of(this.year, this.monthOfYear, this.dayOfMonth)
 }
+
+fun LocalDate.toDateTime(): DateTime {
+    return DateTime(this.year, this.monthValue, this.dayOfMonth, 0, 0, 0)
+}
--- src/main/kotlin/com/marcfearby/view/BudgetTrackerWorkspace.kt
+++ src/main/kotlin/com/marcfearby/view/BudgetTrackerWorkspace.kt
@@ -1,10 +1,16 @@
 package com.marcfearby.view
 
+import com.marcfearby.model.ExpensesEntryTable
 import com.marcfearby.util.createTables
 import com.marcfearby.util.enableConsoleLogger
+import com.marcfearby.util.execute
+import com.marcfearby.util.toDateTime
 import javafx.scene.control.TabPane
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.insert
 import tornadofx.*
+import java.math.BigDecimal
+import java.time.LocalDate
 
 class BudgetTrackerWorkspace: Workspace("Budget Tracker Workspace", NavigationMode.Tabs) {
 
@@ -12,6 +18,14 @@ class BudgetTrackerWorkspace: Workspace("Budget Tracker Workspace", NavigationMo
         enableConsoleLogger()
         Database.connect("jdbc:sqlite:./budget-tracker.db", "org.sqlite.JDBC")
         createTables()
+
+        execute {
+            ExpensesEntryTable.insert {
+                it[entryDate] = LocalDate.now().toDateTime()
+                it[itemName] = "Candy"
+                it[itemPrice] = BigDecimal.valueOf(5.95)
+            }
+        }
 
         tabContainer.tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
     }
