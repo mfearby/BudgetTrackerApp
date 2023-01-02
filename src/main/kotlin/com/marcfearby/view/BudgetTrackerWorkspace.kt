db95766d12d9f40618eec2d8b4f70355f0f9e735
Marc Fearby <marc.fearby@service.nsw.gov.au>
2023-01-02 13:47:42 +1100

    Create the database/table on startup

--- src/main/kotlin/com/marcfearby/view/BudgetTrackerWorkspace.kt
+++ src/main/kotlin/com/marcfearby/view/BudgetTrackerWorkspace.kt
@@ -1,11 +1,18 @@
 package com.marcfearby.view
 
+import com.marcfearby.util.createTables
+import com.marcfearby.util.enableConsoleLogger
 import javafx.scene.control.TabPane
+import org.jetbrains.exposed.sql.Database
 import tornadofx.*
 
 class BudgetTrackerWorkspace: Workspace("Budget Tracker Workspace", NavigationMode.Tabs) {
 
     init {
+        enableConsoleLogger()
+        Database.connect("jdbc:sqlite:./budget-tracker.db", "org.sqlite.JDBC")
+        createTables()
+
         tabContainer.tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
     }
 }
