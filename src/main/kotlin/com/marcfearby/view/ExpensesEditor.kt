79815c013a776af3a33a2072326045dfa9ac1574
Marc Fearby <marc.fearby@service.nsw.gov.au>
2023-01-03 20:07:21 +1100

    Add field validation (although I was able to save a dodgy date!)

--- src/main/kotlin/com/marcfearby/view/ExpensesEditor.kt
+++ src/main/kotlin/com/marcfearby/view/ExpensesEditor.kt
@@ -16,22 +16,53 @@ class ExpensesEditor: View("Expenses") {
                 fieldset {
                     field("Entry date") {
                         maxWidth = 220.0
-                        datepicker(model.entryDate)
+                        datepicker(model.entryDate) {
+                            this.required()
+                            validator {
+                                when {
+                                    it?.dayOfMonth.toString().isEmpty()
+                                            || it?.monthValue.toString().isEmpty()
+                                            || it?.year.toString().isEmpty() -> error("Date is required")
+                                    else -> null
+                                }
+                            }
+                        }
                     }
                     field("Item name") {
                         maxWidth = 220.0
-                        textfield(model.itemName)
+                        textfield(model.itemName) {
+                            this.required()
+                            validator {
+                                when {
+                                    it.isNullOrEmpty() -> error("Item name is required")
+                                    it.length < 3 -> error("Item name is too short")
+                                    else -> null
+                                }
+                            }
+                        }
                     }
                     field("Item price") {
                         maxWidth = 220.0
-                        textfield(model.itemPrice)
+                        textfield(model.itemPrice) {
+                            this.required()
+                            validator {
+                                when(it) {
+                                    null -> error("Item price is required")
+                                    else -> null
+                                }
+                            }
+                        }
                     }
                 }
 
                 hbox(10.0) {
                     button("Add Item") {
+                        enableWhen(model.valid)
                         action {
-                            addItem()
+                            model.commit {
+                                addItem()
+                                model.rollback()
+                            }
                         }
                     }
                     button("Delete Item") {
